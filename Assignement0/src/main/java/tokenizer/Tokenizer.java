db2fbd13051be7f6e6349a171a20d7bceec7e566
Marre24 <Maximilian.ellnestam@gmail.com>
2025-11-07 12:31:11 +0100

    Add valid checker for tokenizer

--- Assignement0/src/main/java/tokenizer/Tokenizer.java
+++ Assignement0/src/main/java/tokenizer/Tokenizer.java
@@ -35,37 +35,69 @@ public class Tokenizer implements ITokenizer{
     public void moveNext() throws IOException, TokenizerException {
         scanner.moveNext();
         char current = scanner.current();
+
+        while (!isValid(current)){
+            scanner.moveNext();
+            current = scanner.current();
+        }
+
         Token token = Token.EOF;
 
-        if (current == Scanner.NULL)
-            token = Token.NULL;
+        switch (current){
+            case Scanner.EOF:
+                this.current = new Lexeme(String.valueOf(Scanner.EOF), token);
+                return;
+            case Scanner.NULL:
+                token = Token.NULL;
+                break;
+            case ADD_OP:
+                token = Token.ADD_OP;
+                break;
+            case SUB_OP:
+                token = Token.SUB_OP;
+                break;
+            case MULT_OP:
+                token = Token.MULT_OP;
+                break;
+            case DIV_OP:
+                token = Token.DIV_OP;
+                break;
+            case ASSIGN_OP:
+                token = Token.ASSIGN_OP;
+                break;
+            case LEFT_PAREN:
+                token = Token.LEFT_PAREN;
+                break;
+            case RIGHT_PAREN:
+                token = Token.RIGHT_PAREN;
+                break;
+            case SEMICOLON:
+                token = Token.SEMICOLON;
+                break;
+            case LEFT_CURLY:
+                token = Token.LEFT_CURLY;
+                break;
+            case RIGHT_CURLY:
+                token = Token.RIGHT_CURLY;
+                break;
+        }
+
         if (Character.isDigit(current))
             token = Token.INT_LIT;
         if (Character.isAlphabetic(current))
             token = Token.IDENT;
-        if (current == ADD_OP)
-            token = Token.ADD_OP;
-        if (current == SUB_OP)
-            token = Token.SUB_OP;
-        if (current ==  MULT_OP)
-            token = Token. MULT_OP;
-        if (current == DIV_OP)
-            token = Token.DIV_OP;
-        if (current == ASSIGN_OP)
-            token = Token.ASSIGN_OP;
-        if (current == LEFT_PAREN)
-            token = Token.LEFT_PAREN;
-        if (current == RIGHT_PAREN)
-            token = Token.RIGHT_PAREN;
-        if (current == SEMICOLON)
-            token = Token.SEMICOLON;
-        if (current == LEFT_CURLY)
-            token = Token.LEFT_CURLY;
-        if (current == RIGHT_CURLY)
-            token = Token.RIGHT_CURLY;
 
+        this.current = new Lexeme(String.valueOf(current), token);
+    }
 
-        this.current = new Lexeme(current, token);
+    private boolean isValid(char c){
+        if (Character.isDigit(c) || Character.isAlphabetic(c))
+            return true;
+        return switch (c) {
+            case Scanner.EOF, ADD_OP, Scanner.NULL, SUB_OP, MULT_OP, DIV_OP, ASSIGN_OP, LEFT_PAREN, RIGHT_PAREN,
+                 SEMICOLON, LEFT_CURLY, RIGHT_CURLY -> true;
+            default -> false;
+        };
     }
 
     @Override
--- Assignement0/src/test/java/TokenizerTest.java
+++ Assignement0/src/test/java/TokenizerTest.java
@@ -1,21 +1,25 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import scanner.Scanner;
 import tokenizer.Lexeme;
 import tokenizer.Token;
 import tokenizer.Tokenizer;
 import tokenizer.TokenizerException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TokenizerTest {
 
+    private final static String[] PROGRAM_1_LEXEMES = {"a", "=", "1", "*", "2", "+", "(", "3", "-", "4", ")", "/", "5", ";"};
+
     private final static String PROGRAM_1_PATH = "src/main/resources/program1.txt";
     private final static String PROGRAM_2_PATH = "src/main/resources/program2.txt";
 
-    private final static Lexeme PROGRAM_1_FIRST_LEXEME = new Lexeme('a', Token.IDENT);
+    private final static Lexeme PROGRAM_1_FIRST_LEXEME = new Lexeme("a", Token.IDENT);
 
     private Tokenizer tokenizer;
 
@@ -31,21 +35,55 @@ public class TokenizerTest {
     }
 
     @Test
-    public void currentLexemeIsNotNull(){
+    public void currentLexemeIsNotNull() {
         assertNotNull(tokenizer.current());
     }
 
     @Test
-    public void currentLexemeValueEqualsFirstLexemeValue(){
+    public void currentLexemeValueEqualsFirstLexemeValue() {
         var actual = tokenizer.current();
 
         assertEquals(PROGRAM_1_FIRST_LEXEME.value(), actual.value());
     }
 
     @Test
-    public void currentLexemeTokenEqualsFirstLexemeToken(){
+    public void currentLexemeTokenEqualsFirstLexemeToken() {
         var actual = tokenizer.current();
 
         assertEquals(PROGRAM_1_FIRST_LEXEME.token(), actual.token());
+    }
+
+    @Test
+    public void totalLexemesIsCorrect() throws TokenizerException, IOException {
+        var current = tokenizer.current();
+        int i = 0;
+
+        while (current.value() instanceof String str && !str.equals(String.valueOf(Scanner.EOF))){
+            i++;
+            tokenizer.moveNext();
+            current = tokenizer.current();
+        }
+
+        assertEquals(PROGRAM_1_LEXEMES.length, i);
+    }
+
+    @Test
+    public void lexemesIsCorrect() throws TokenizerException, IOException {
+        var current = tokenizer.current();
+
+        StringBuilder actual = new StringBuilder();
+
+        while (current.value() instanceof String str && !str.equals(String.valueOf(Scanner.EOF))){
+            actual.append(current.value().toString()).append(";");
+            tokenizer.moveNext();
+            current = tokenizer.current();
+        }
+
+        StringBuilder expected = new StringBuilder();
+
+        for (String s : PROGRAM_1_LEXEMES)
+            expected.append(s).append(";");
+
+        assertEquals(expected.toString(), actual.toString());
     }
 }
