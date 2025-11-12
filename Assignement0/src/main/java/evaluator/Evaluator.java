38c845283591ae5c95dd2a684db1a6ef321b8a33
Marre24 <Maximilian.ellnestam@gmail.com>
2025-11-12 10:12:49 +0100

    Remove evaluator class

--- Assignement0/src/main/java/evaluator/Evaluator.java
+++
@@ -1,4 +0,0 @@
-package evaluator;
-
-public class Evaluator {
-}
--- Assignement0/src/test/java/EvaluationTest.java
+++ Assignement0/src/test/java/EvaluationTest.java
@@ -1,6 +1,5 @@
 import Nodes.INode;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import parser.Parser;
 import parser.ParserException;
@@ -13,11 +12,11 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 public class EvaluationTest {
 
     private final static String PROGRAM_2_PATH = "src/main/resources/program2.txt";
-    private INode root;
+    private static INode root;
 
-    @BeforeEach
-    public void parserCanOpenProgram1() {
-        Parser parser  = new Parser();
+    @BeforeAll
+    public static void parserCanOpenProgram1() {
+        Parser parser = new Parser();
         assertDoesNotThrow(() -> parser.open(PROGRAM_2_PATH));
         try {
             root = parser.parse();
@@ -28,7 +27,7 @@ public class EvaluationTest {
     }
 
     @Test
-    public void evaluateDoesNotThrowException(){
+    public void evaluateDoesNotThrowException() {
         Object[] objects = new Object[0];
         assertDoesNotThrow(() -> root.evaluate(objects));
     }
