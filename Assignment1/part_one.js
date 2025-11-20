beccbd09d84e31d949bdfaf068d38e53bf22ace0
Marre24 <Maximilian.ellnestam@gmail.com>
2025-11-20 16:42:14 +0100

    Refactor

--- Assignment1/part_one.js
+++ Assignment1/part_one.js
@@ -3,13 +3,17 @@ const couldNotFind = "Could Not Find Method";
 var myObject = {
   create: function (prototypeList) {
     var t = {};
+
     t.prototypeList = prototypeList;
     if (t.prototypeList === null || t.prototypeList === undefined)
       t.prototypeList = [];
+
     t.create = this.create;
     t.call = this.call;
     t.addPrototype = this.addPrototype;
     t.hasPrototype = this.hasPrototype;
+    t.checkForPrototypeFunctions = this.checkForPrototypeFunctions;
+
     return t;
   },
 
@@ -21,23 +25,18 @@ var myObject = {
 
   hasPrototype: function (prototype) {
     for (const p of this.prototypeList) {
-      if (p.hasPrototype(prototype)) return true;
       if (p === prototype) return true;
+      if (p.hasPrototype(prototype)) return true;
     }
     return false;
   },
 
-  call: function (funcName, parameters) {
-    let visited = [];
-
-    if (arguments.length >= 3) visited = arguments[2];
-
-    if (visited.includes(this)) return couldNotFind;
-
-    visited.push(this);
-
+  call: function (funcName, parameters, visited = []) {
     if (typeof this[funcName] === "function")
       return this[funcName](...parameters);
+
+    if (visited.includes(this)) return couldNotFind;
+    visited.push(this);
 
     for (const prototype of this.prototypeList) {
       let result = prototype.call(funcName, parameters, visited.slice());
