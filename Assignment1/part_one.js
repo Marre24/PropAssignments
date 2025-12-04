c63a1396c4dc1f46d4ed3d4dd9b04ad2017f15a1
Marre24 <Maximilian.ellnestam@gmail.com>
2025-12-04 12:59:00 +0100

    Refactor objects dont have their own copy of functions

--- Assignment1/part_one.js
+++ Assignment1/part_one.js
@@ -1,5 +1,36 @@
 const couldNotFind = "Could Not Find Method";
 
+call = function (funcName, parameters, receiver) {
+  if (receiver === undefined) receiver = this;
+
+  if (typeof this[funcName] === "function")
+    return this[funcName].apply(receiver, parameters);
+
+  for (const prototype of this.prototypeList) {
+    let result = prototype.call(funcName, parameters, receiver);
+
+    if (result === couldNotFind) continue;
+
+    return result;
+  }
+
+  return couldNotFind;
+};
+
+addPrototype = function (prototype) {
+  if (prototype === this || prototype.hasPrototype(this))
+    throw Error("will result in circular prototypes");
+  this.prototypeList.push(prototype);
+};
+
+hasPrototype = function (prototype) {
+  for (const p of this.prototypeList) {
+    if (p === prototype) return true;
+    if (p.hasPrototype(prototype)) return true;
+  }
+  return false;
+};
+
 var myObject = {
   create: function (prototypeList) {
     var t = {};
@@ -10,36 +41,9 @@ var myObject = {
 
     t.create = this.create;
 
-    t.call = function (funcName, parameters, receiver) {
-      if (receiver === undefined) receiver = this;
-
-      if (typeof this[funcName] === "function")
-        return this[funcName].apply(receiver, parameters);
-
-      for (const prototype of this.prototypeList) {
-        let result = prototype.call(funcName, parameters, receiver);
-
-        if (result === couldNotFind) continue;
-
-        return result;
-      }
-
-      return couldNotFind;
-    };
-
-    t.addPrototype = function (prototype) {
-      if (prototype === this || prototype.hasPrototype(this))
-        throw Error("will result in circular prototypes");
-      this.prototypeList.push(prototype);
-    };
-
-    t.hasPrototype = function (prototype) {
-      for (const p of this.prototypeList) {
-        if (p === prototype) return true;
-        if (p.hasPrototype(prototype)) return true;
-      }
-      return false;
-    };
+    t.call = call;
+    t.addPrototype = addPrototype;
+    t.hasPrototype = hasPrototype;
 
     return t;
   },
--- Assignment1/part_two.js
+++ Assignment1/part_two.js
@@ -1,5 +1,60 @@
 const couldNotFind = "Could Not Find Method";
 
+call = function (funcName, parameters) {
+  if (typeof this[funcName] === "function")
+    return this[funcName].apply(this, parameters);
+
+  return this.__class__.checkForClassMethods(
+    this.__class__,
+    funcName,
+    parameters,
+    this
+  );
+};
+
+constructor = function () {
+  var t = {};
+
+  t.__class__ = this;
+
+  t.call = call;
+
+  return t;
+};
+addSuperClass = function (superClass) {
+  if (superClass === this || superClass.hasSuperClass(this))
+    throw Error("will result in circular class inheritance");
+  this.superClassList.push(superClass);
+};
+
+hasSuperClass = function (superClass) {
+  for (const c of this.superClassList) {
+    if (c === superClass) return true;
+    if (c.hasSuperClass(superClass)) return true;
+  }
+  return false;
+};
+
+checkForClassMethods = function (currentClass, funcName, parameters, receiver) {
+  if (typeof currentClass[funcName] === "function")
+    return currentClass[funcName].apply(receiver, parameters);
+
+  for (const superClass of currentClass.superClassList) {
+    let result = this.checkForClassMethods(
+      superClass,
+      funcName,
+      parameters,
+      receiver
+    );
+
+    if (result === couldNotFind) continue;
+
+    return result;
+  }
+
+  return couldNotFind;
+};
+
 createClass = function (className, superClassList) {
   if (superClassList === null || superClassList === undefined)
     superClassList = [];
@@ -8,63 +63,9 @@ createClass = function (className, superClassList) {
     className: className,
     superClassList: superClassList,
 
-    new: function () {
-      var t = {};
-
-      t.__class__ = this;
-
-      t.call = function (funcName, parameters) {
-        if (typeof this[funcName] === "function")
-          return this[funcName].apply(this, parameters);
-
-        return this.__class__.checkForClassMethods(
-          this.__class__,
-          funcName,
-          parameters,
-          this
-        );
-      };
-
-      return t;
-    },
-
-    addSuperClass: function (superClass) {
-      if (superClass === this || superClass.hasSuperClass(this))
-        throw Error("will result in circular class inheritance");
-      this.superClassList.push(superClass);
-    },
-
-    hasSuperClass: function (superClass) {
-      for (const c of this.superClassList) {
-        if (c === superClass) return true;
-        if (c.hasSuperClass(superClass)) return true;
-      }
-      return false;
-    },
-
-    checkForClassMethods: function (
-      currentClass,
-      funcName,
-      parameters,
-      receiver
-    ) {
-      if (typeof currentClass[funcName] === "function")
-        return currentClass[funcName].apply(receiver, parameters);
-
-      for (const superClass of currentClass.superClassList) {
-        let result = this.checkForClassMethods(
-          superClass,
-          funcName,
-          parameters,
-          receiver
-        );
-
-        if (result === couldNotFind) continue;
-
-        return result;
-      }
-
-      return couldNotFind;
-    },
+    new: constructor,
+    addSuperClass: addSuperClass,
+    hasSuperClass: hasSuperClass,
+    checkForClassMethods: checkForClassMethods,
   };
 };
