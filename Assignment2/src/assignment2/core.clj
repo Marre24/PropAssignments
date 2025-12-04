de6135037bfdc7eab219131b64abf1b496684bdb
Marre24 <Maximilian.ellnestam@gmail.com>
2025-12-04 17:10:11 +0100

    Add my-max function

--- Assignment2/src/assignment2/core.clj
+++ Assignment2/src/assignment2/core.clj
@@ -1,7 +1,9 @@
 (ns assignment2.core
-  (:require [assignment2.functional :refer [helloWorld]]))
+  (:require [assignment2.functional :refer [my-max]]))
 
 
 
 (defn -main []
-  (helloWorld))
+  (println (my-max '(1 2 3 55 4 5 6)))
+  (println (my-max '(36)))
+  (println (my-max '())))
--- Assignment2/src/assignment2/functional.clj
+++ Assignment2/src/assignment2/functional.clj
@@ -1,5 +1,13 @@
 (ns assignment2.functional)
 
-(defn helloWorld
-  []
-  (println "Hello World!"))
+(defn my-max
+  ([l]
+   (if (empty? l)
+     nil
+     (my-max (first l) (rest l))))
+  ([current-max l]
+   (if (empty? l)
+     current-max
+     (if (< current-max (first l))
+       (recur (first l) (rest l))
+       (recur current-max (rest l))))))
