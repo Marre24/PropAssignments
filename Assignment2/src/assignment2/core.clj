c229a8e98a5aaae19f450b7f183a7c0fd38b5cb5
Marre24 <Maximilian.ellnestam@gmail.com>
2025-12-04 17:25:04 +0100

    Add my-map function

--- Assignment2/src/assignment2/core.clj
+++ Assignment2/src/assignment2/core.clj
@@ -1,9 +1,12 @@
 (ns assignment2.core
-  (:require [assignment2.functional :refer [my-max]]))
+  (:require [assignment2.functional :refer [my-max my-map]]))
 
 
 
 (defn -main []
   (println (my-max '(1 2 3 55 4 5 6)))
   (println (my-max '(36)))
-  (println (my-max '())))
+  (println (my-max '()))
+  (println (my-map '(1 2 3 4 5 6) inc))
+  (println (my-map '(-3 6 -9 12) pos?)))
+
--- Assignment2/src/assignment2/functional.clj
+++ Assignment2/src/assignment2/functional.clj
@@ -11,3 +11,11 @@
      (if (< current-max (first l))
        (recur (first l) (rest l))
        (recur current-max (rest l))))))
+
+(defn my-map
+  ([list func]
+   (my-map list func '()))
+  ([list func result]
+   (if (empty? list)
+     (reverse result)
+     (recur (rest list) func (conj result (func (first list)))))))
